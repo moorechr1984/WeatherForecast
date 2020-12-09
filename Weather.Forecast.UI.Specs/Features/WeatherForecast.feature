6f770b878acc9528f83da53a9753a180c8c9a55f
Chris Moore <Chris.Moore@meijer.com>
2020-12-09 14:38:36 -0500

    added files

---
+++ Weather.Forecast.UI.Specs/Features/WeatherForecast.feature
@@ -0,0 +1,13 @@
+﻿Feature: WeatherForecast	
+As a user, I want to view today's forecasted temperature, so that i know what to wear today.
+
+@mytag
+Scenario: View Today's forecast
+	Given the user is viewing the home page
+	When he/she clicks today's forecast
+	Then he should see "It Worked" 
+
+	Given the user is viewing the home page
+	When he/she clicks today's forecast
+	And clicks Clear Me
+	Then he should see """" 
---
+++ Weather.Forecast.UI.Specs/Steps/WeatherForecastSteps.cs
@@ -0,0 +1,48 @@
+﻿using AngleSharp.Dom;
+using Bunit;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Weather.Forecast.UI.Specs.Steps
+{
+    [Binding]
+    public class WeatherForecastSteps
+    {
+        private TestContext _testContext = new TestContext();
+        private IRenderedComponent<Pages.Index> _mainLayout;
+        private IElement _btnTodaysForecast;
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public WeatherForecastSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"the user is viewing the home page")]
+        public void GivenTheUserIsViewingTheHomePage()
+        {
+            _mainLayout = _testContext.RenderComponent<Pages.Index>();
+        }
+        
+        [When(@"he/she clicks today's forecast")]
+        public void WhenHeSheClicksTodaySForecast()
+        {
+            _btnTodaysForecast = _mainLayout.Find("#btnTodaysForecast");
+            _btnTodaysForecast.Click();
+        }
+
+        [When(@"clicks Clear Me")]
+        public void WhenClicksClearMe()
+        {
+            _btnTodaysForecast = _mainLayout.Find("#btnTodaysForecastCleared");
+            _btnTodaysForecast.Click();
+        }
+
+        [Then(@"he should see ""(.*)""")]
+        public void ThenHeShouldSee(string p0)
+        {
+            var divResult = _mainLayout.Find("#divResult");
+            Equals(p0, divResult.InnerHtml);
+        }
+    }
+}
